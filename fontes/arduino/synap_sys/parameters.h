2efc37404acbe55b69d5c7102c6b5fbcfc4c3f37
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-07-26 16:40:30 -0300

    primeira versão para testes nesta versão inda há erros a serem corrigidos já identificados Há dois arquivos o synap_sys.ino que o padrão do arduino e contem a logica principal, o arquivo parameters.h possui informações de parametrização e também para depuraçao em versões futuras as informações de depruação serão movidas para um arquivo especifico como debug.h

--- fontes/arduino/synap_sys/parameters.h
+++ fontes/arduino/synap_sys/parameters.h
@@ -1,8 +1,25 @@
 #include "Arduino.h"
 
-#define DEBUG (true)
+// macros para ativacao de depuracao
+#define D_L_INIT   B00000001
+#define D_L_OUTPUT B00001000
+#define D_L_PAR    B00010000
+#define D_L_INPUT  B00100000
+//#define DEBUG         ( D_L_INIT | D_L_OUTPUT | D_L_PAR | D_L_INPUT)
+#define DEBUG         ( D_L_OUTPUT | D_L_PAR )
+//#define DEBUG         (0)
+#define DEBUG_INIT    (D_L_INIT   == (DEBUG & D_L_INIT ))
+#define DEBUG_OUTPUT  (D_L_OUTPUT == (DEBUG & D_L_OUTPUT))
+#define DEBUG_PAR     (D_L_PAR    == (DEBUG & D_L_PAR))
+#define DEBUG_INPUT   (D_L_INPUT  == (DEBUG & D_L_INPUT))
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
+
+// identificacao dos pinos usados, 
+// o mapeamento nao e direto o pino 1 nao e necessariamente o mesmo pino no arduino
+#if DEBUG > 0
+const byte PINS[]    = {2, 3, 4, 5, 6, 7};
+const byte POTS[]    = {A1, A2, A3, A4};
+#elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1284__) || defined(__AVR_ATmega1284P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)
 const byte PINS[]    = {2, 3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 37};
 const byte POTS[]    = {A1, A2, A3, A4};
 #else
@@ -12,7 +29,29 @@ const byte POTS[]    = {A1, A2, A3, A4};
 
 const byte NUM_PINS  = sizeof(PINS) / sizeof(*PINS);
 const byte NUM_POTS  = sizeof(POTS) / sizeof(*POTS);
-const byte NUM_PARES = NUM_PINS/2;
+const byte NUM_PARES = NUM_PINS / 2;
+
+// DEFINICAO DO ARRAY QUE GUARDA PARES CONECTADOS
+#define PIN_1 0
+#define PIN_2 1
+byte pares[NUM_PARES][2];
+#define parFree(par) (pares[par][0] == 255 || pares[par][1] == 255)
+#define parUsed(par) (!parFree(par))
+#define parPin1(par) (pares[par][0])
+#define parPin2(par) (pares[par][1])
+#define PARES(par,pin_1,pin_2) do{\
+    pares[par][PIN_1] = pin_1;\
+    pares[par][PIN_2] = pin_2;\
+  }while(false);
+#define usedPin(par,pin)  (pares[par][PIN_1] == pin || pares[par][PIN_2] == pin)
+
+
+
+
+#define DELAY_OUTPUT (DEBUG?100:0)
+#define DELAY_INPUT (DEBUG?100:0)
+#define DELAY_LOOP (DEBUG?500:30)
+
 
 
 
--- fontes/arduino/synap_sys/synap_sys.ino
+++ fontes/arduino/synap_sys/synap_sys.ino
@@ -1,53 +1,109 @@
 #include "parameters.h"
 
+String str;
+
 void setup() {
   Serial.begin(9660);
-  if (DEBUG) {
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
-    Serial.println("");
+  Serial.println("\n\n\n\n\n/init/begin");
+
+  if (DEBUG_INIT) {
     Serial.println("/DEBUG/ENABLED");
   }
+
+  if (DEBUG_INIT) {
+    str = "/DEBUG/NUM_PINS/";
+    str += NUM_PINS;
+    Serial.println(str);
+    str = "/DEBUG/NUM_POTS/";
+    str += NUM_POTS;
+    Serial.println(str);
+  }
+
+
+  Serial.println("\n/init/pins/check");
   for (byte b = 0; b < NUM_PINS; b++) {
+
+    pinMode(PINS[b], OUTPUT);
+    digitalWrite(PINS[b], LOW);
     pinMode(PINS[b], INPUT);
+    if (DEBUG_INIT) {
+      str = "/DEBUG/PIN/";
+      str += b;
+      str += "/INPUT/";
+      str += digitalRead(PINS[b]) ? "ON" : "OFF";
+      Serial.println(str);
+    }
+  }
+  Serial.println("\n/init/pins/ok");
+
+  if (DEBUG_INIT) {
+    Serial.println("/DEBUG/STARTED\n");
   }
 
-  if (DEBUG) {
-    Serial.print("/DEBUG/NUM_PINS/");
-    Serial.println(NUM_PINS);
-    Serial.print("/DEBUG/NUM_POTS/");
-    Serial.println(NUM_POTS);
-  }
+  str = "\n/init/delays/";
+  str += DELAY_INPUT;
+  str += "/";
+  str += DELAY_OUTPUT;
+  str += "/";
+  str += DELAY_LOOP;
+  Serial.println(str);
+
+  Serial.println("\n/init/end");
 }
-void loop() {
 
-  byte pares[NUM_PARES][2];
-  byte c = 0;
-  for (byte b = 0; b < NUM_PARES; b++) {
-    pares[b][0] = 255;
-    pares[b][1] = 255;
-  }
+
+byte c;
+
+void loop() {
+  str = "/loop/";
+  str += millis();
+  Serial.println(str);
+
+  c = 0;
+
+  resetPares();
+
 
   for (byte p_out = 0; p_out < NUM_PINS && c < NUM_PARES; p_out++) {
+    delay(DELAY_OUTPUT);
+
     bool nextOutput = false;
     pinMode(PINS[p_out], OUTPUT);
     digitalWrite(PINS[p_out], HIGH);
-    if (DEBUG) {
-      Serial.print("/DEBUG/PIN/OUTPUT/");
-      Serial.println(p_out);
-    }
+
+    if (DEBUG > 10) {
+      str = "\n/DEBUG/PIN/OUTPUT/";
+      str += p_out;
+      Serial.println(str);
+    } // DEBUG
 
     for (byte p_in = 0; p_in < NUM_PINS; p_in++) {
       if (p_in == p_out) continue;
+
+      delay(DELAY_INPUT);
+
       bool nextInput = false;
-      if (DEBUG) {
-        Serial.print("/DEBUG/PIN/INPUT/");
-        Serial.println(p_in);
-      }
+
+      if (DEBUG_INPUT) {
+        str = "/DEBUG/PIN/";
+        str += p_in;
+        str += "/INPUT/";
+        str += digitalRead(PINS[p_in]) ? "ON" : "OFF";
+        Serial.println(str);
+      } // DEBUG
 
       for (byte par = 0; par < NUM_PARES  && c < NUM_PARES; par++) {
-        if (pares[par][0] == p_in || pares[par][1] == p_in) {
+        if (DEBUG_PAR) {
+          str = "/DEBUG/PAR/";
+          str += par;
+          str += "/";
+          str += parPin1(par);
+          str += "/";
+          str += parPin2(par);
+          Serial.println(str);
+        }// DEBUG
+
+        if (usedPin(par,p_in)) {
           nextInput = true;
           break;
         }
@@ -55,16 +111,42 @@ void loop() {
       if (nextInput) continue;
 
       if (digitalRead(PINS[p_in])) {
-        if (DEBUG) {
-          Serial.print("/DEBUG/PIN/ON/");
-          Serial.println(p_in);
-        }
-        pares[c][0] = p_in;
-        pares[c++][1] = p_out;
+        if (DEBUG > 5) {
+          str = "\n/DEBUG/PIN/ON/";
+          str += p_out;
+          str += "/WITH/";
+          str += p_in;
+          str += "\n";
+          Serial.println(str);
+        } // DEBUG
+
+        checkPar(p_in, p_out);
+
         nextOutput = true; // vou para a proxima entrada, apenas um par por output;
         break;
       }
-    }
+    } //FINDA FOR INPUT
+
+    digitalWrite(PINS[p_out], LOW);
     if (nextOutput) continue;
+  } // FINDA FOR OUTPUT
+  delay(DELAY_LOOP);
+}
+
+
+inline void resetPares() {
+  for (byte b = 0; b < NUM_PARES; b++) {
+    PARES(b, 255, 255);
   }
+}
+
+void checkPar(byte p_in, byte p_out) {
+  PARES(c++, p_in, p_out);
+
+  str = "/PIN/";
+  str += p_in;
+  str += "/";
+  str += p_out;
+  str += "/ON";
+  Serial.println(str);
 }
